a1f70a82fc004be01f8571ddf15dd41030469e71
saba torkaman <s.torkaman.kh@gmail.com>
2025-07-01 13:59:10 +0330

    add form signup

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,13 +1,14 @@
-import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
-import { provideRouter } from '@angular/router';
-import { routes } from './app.routes';
-import { provideClientHydration, withEventReplay } from '@angular/platform-browser';
-import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
-import { providePrimeNG } from 'primeng/config';
+import {ApplicationConfig, provideZoneChangeDetection} from '@angular/core';
+import {provideRouter} from '@angular/router';
+import {routes} from './app.routes';
+import {provideClientHydration, withEventReplay} from '@angular/platform-browser';
+import {provideAnimationsAsync} from '@angular/platform-browser/animations/async';
+import {providePrimeNG} from 'primeng/config';
 import Aura from '@primeng/themes/aura';
+import {provideHttpClient} from '@angular/common/http';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }),
+  providers: [provideZoneChangeDetection({eventCoalescing: true}),
     provideAnimationsAsync(),
     providePrimeNG({
       theme: {
@@ -15,5 +16,6 @@ export const appConfig: ApplicationConfig = {
       }
     }),
     provideRouter(routes),
+    provideHttpClient(),
     provideClientHydration(withEventReplay())]
 };
--- src/app/signup/signup.component.ts
+++ src/app/signup/signup.component.ts
@@ -1,5 +1,14 @@
-import {Component, inject} from '@angular/core';
-import {FormBuilder, FormGroup, FormsModule, ReactiveFormsModule} from '@angular/forms';
+import {Component, inject, OnInit} from '@angular/core';
+import {
+  AbstractControl,
+  FormBuilder,
+  FormGroup,
+  FormsModule,
+  ReactiveFormsModule,
+  ValidationErrors,
+  ValidatorFn,
+  Validators
+} from '@angular/forms';
 import {InputTextModule} from 'primeng/inputtext';
 import {ActivatedRoute} from '@angular/router';
 import {FloatLabel} from 'primeng/floatlabel';
@@ -8,25 +17,94 @@ import {IconField} from 'primeng/iconfield';
 import {InputGroup} from 'primeng/inputgroup';
 import {InputGroupAddon} from 'primeng/inputgroupaddon';
 import {Button} from 'primeng/button';
+import {DatePicker} from 'primeng/datepicker';
+import {CardModule} from 'primeng/card';
+import {SelectButton} from 'primeng/selectbutton';
+import {ApiService, GetCountryResponse} from '../api.service';
+import {Select} from 'primeng/select';
+import {MultiSelectModule} from 'primeng/multiselect';
+import {DatePipe, JsonPipe} from '@angular/common';
+
+export const confirmPasswordValidator: ValidatorFn = (
+  control: AbstractControl
+): ValidationErrors | null => {
+  console.log(control.value.password === control.value.repeatPassword)
+  return control.value.password === control.value.repeatPassword
+    ? null
+    : {PasswordNoMatch: true};
+};
 
 @Component({
   selector: 'app-signup',
-  imports: [InputTextModule, FormsModule, ReactiveFormsModule, FloatLabel, InputIcon, IconField, InputGroup, InputGroupAddon, Button],
+  imports: [InputTextModule, MultiSelectModule, CardModule, Select, FormsModule, DatePicker, ReactiveFormsModule, FloatLabel, InputIcon, IconField, InputGroup, InputGroupAddon, Button, SelectButton, DatePipe, JsonPipe],
   templateUrl: './signup.component.html',
   styleUrl: './signup.component.scss'
 })
-export class SignupComponent {
+export class SignupComponent implements OnInit {
   private route = inject(ActivatedRoute)
-  signupForm: FormGroup
   private fb = inject(FormBuilder)
+  private api = inject(ApiService)
+  stateOptions: any[] = [
+    {value: 'male', logo: 'pi pi-mars'},
+    {value: 'female', logo: 'pi pi-venus'}
+  ];
+
+  signupForm: FormGroup
   showpass = false
-  showpasstype = 'password'
+  countries?: GetCountryResponse
+  submittedData: any = null;
 
   constructor() {
-    this.signupForm = this.fb.group({})
+    this.signupForm = this.fb.group({
+      username: this.fb.control("", [
+        Validators.required,
+        Validators.minLength(4)
+
+      ]),
+      email: this.fb.control("", [
+        Validators.required,
+        Validators.email
+      ]),
+      password: this.fb.control("", [
+        Validators.required,
+        Validators.minLength(4),
+        Validators.maxLength(10)
+
+      ]),
+      repeatPassword: this.fb.control("", [
+        Validators.required,
+        Validators.maxLength(10),
+        Validators.minLength(4)
+      ]),
+      date: this.fb.control("", [
+        Validators.required
+      ]),
+      gender: this.fb.control("", [
+        Validators.required
+      ]),
+      birthplace: this.fb.control("", [
+        Validators.required
+      ]),
+      favoriteCountries: this.fb.control("", [
+        Validators.required
+      ])
+    }, {validators: confirmPasswordValidator})
+  }
+
+  ngOnInit() {
+    this.api.getcountry().subscribe((country) => this.countries = country)
   }
 
   showPassword() {
     this.showpass = !this.showpass
+  }
+
+  submit() {
+    if (this.signupForm.valid && this.signupForm.get("repeatPassword")?.value == this.signupForm.get("password")?.value) {
+      this.submittedData = this.signupForm.value;
+    } else {
+      this.submittedData = null;
+      this.signupForm.markAllAsTouched();
+    }
   }
 }
